4a1b15b783488fbb5424fe83cd30ec45e5367928
System Administrator <root@david---macbook-pro-m1.home>
2024-02-11 14:29:39 +0100

    Repair  script for creating table student - duplicate entry when re-running the import script

--- DDL/Tables/student.sql
+++ DDL/Tables/student.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS student (
 	student_id INT PRIMARY KEY AUTO_INCREMENT,
-	student_name VARCHAR(55),
-	student_surname VARCHAR(55),
+	student_name VARCHAR(55) NOT NULL,
+	student_surname VARCHAR(55) NOT NULL,
 	student_birth_date DATE,
-	student_presentation VARCHAR(255)
+	student_presentation VARCHAR(255),
+	UNIQUE KEY unique_student_name_surname_birth (student_name, student_surname, student_birth_date)
 	);
