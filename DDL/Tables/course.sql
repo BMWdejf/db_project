385cbd4308b04486983bb3e3e48ef56659e619b6
System Administrator <root@david---macbook-pro-m1.home>
2024-02-11 11:18:09 +0100

    Add script for creating table course

---
+++ DDL/Tables/course.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS course (
+	course_id INT PRIMARY KEY AUTO_INCREMENT,
+	course_name VARCHAR(55),
+	course_description VARCHAR(255),
+	course_start_date DATETIME,
+	course_trainer_id INT,
+	FOREIGN KEY (course_trainer_id) REFERENCES trainer(trainer_id)
+);
