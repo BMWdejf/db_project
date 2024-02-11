afb84292a83b5ec6b7e1f22e1934a533d60bc9f3
System Administrator <root@david---macbook-pro-m1.home>
2024-02-11 12:48:12 +0100

    Repair  script for creating table trainer - duplicate entry when re-running the import script

--- DDL/Tables/trainer.sql
+++ DDL/Tables/trainer.sql
@@ -1,8 +1,9 @@
 CREATE TABLE IF NOT EXISTS trainer (
 	trainer_id INT PRIMARY KEY AUTO_INCREMENT,
-	trainer_name VARCHAR(55),
-	trainer_surname VARCHAR(55),
+	trainer_name VARCHAR(55) NOT NULL,
+	trainer_surname VARCHAR(55) NOT NULL,
 	trainer_description VARCHAR(255),
 	trainer_birth_date DATE,
-	trainer_programing_lang VARCHAR(255)
+	trainer_programing_lang VARCHAR(255),
+	UNIQUE KEY unique_trainer_name_surname (trainer_name, trainer_surname)
 );
